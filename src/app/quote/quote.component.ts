ea8b8244a7ba957298723590553e85103facb685
Trinity John <trinityrtime@gmail.com>
2020-01-15 12:00:42 +0300

    input field for author name ;logic defined in components

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -13,49 +13,57 @@ export class QuoteComponent implements OnInit {
       1,
       "Theres never any blood in Star Wars movies because all the lasers and lightsabers would cauterize the wounds instantly.",
       "By MoonWlker",
-      new Date()
+      new Date(),
+      "author"
     ),
     new Quote(
       2,
       "There are many men named Hunter, but oddly no women named gatherer.",
       "By Trinity",
-      new Date(2020, 1, 12)
+      new Date(2020, 1, 12),
+      "author"
     ),
     new Quote(
       3,
       "Turbulence is the pot hole of the sky. ",
       "By Trinity",
-      new Date(2020, 1, 1)
+      new Date(2020, 1, 1),
+      "author"
     ),
     new Quote(
       4,
       "Some innocent pets have given unconditional love to Serial killers.",
       "By Chun Li",
-      new Date(2019, 12, 25)
+      new Date(2019, 12, 25),
+      "author"
     ),
     new Quote(
       5,
       "Blinds are what keep you from being blinded in the morning.",
       "By Myself",
-      new Date(2013, 2, 2)
+      new Date(2013, 2, 2),
+      "author"
     ),
     new Quote(
       6,
       "Rappers who rap fast would have a huge advantage in the Harry Potter universe when casting spells.",
       "By MeGenius",
-      new Date(2020, 1, 13)
+      new Date(2020, 1, 13),
+      "author"
     ),
     new Quote(
       7,
       "You hold shift to run because running is upprcase walking.",
       "By VidGamer",
-      new Date(2002, 12, 29)
+      new Date(2002, 12, 29),
+      "author"
     ),
     new Quote(
       8,
       "Some vampires might get addicted to alcoholics.",
       "By Trevor Ngugi",
-      new Date(2020, 12, 15)
+      new Date(2020, 12, 15),
+      "author"
     )
   ];
 
